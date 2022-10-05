d17379ad5180d8310b67686cd46af6973006e1cb
Elin A <elina@Elins-MacBook-Air.local>
2022-10-05 16:03:35 +0200

    first commit

---
+++ app/src/main/java/com/example/testagit/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.testagit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var a = 10
+        var b = 20
+    }
+}
